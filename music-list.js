50fda1f323fcd4b022de463f73279b16612a7553
AthulStudio <126080930+AthulStudio@users.noreply.github.com>
2023-11-11 08:49:14 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,170 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Heeriye",
+    artist: "jasleen Royal",
+    img: "music_1",
+    src: "music_1"
+  },
+  {
+    name: "neela nilave",
+    artist: "saregama malayalam",
+    img: "music_2",
+    src: "music_2"
+  },
+  {
+    name: "Na Ready",
+    artist: "sony music south",
+    img: "music_3",
+    src: "music_3"
+  },
+  {
+    name: "kalapakkaara",
+    artist: "sony music south",
+    img: "music_4",
+    src: "music_4"
+  },
+  {
+    name: "Hey nima",
+    artist: "fejo",
+    img: "music_5",
+    src: "music_5"
+  },
+  {
+    name: "Nee Singam Dhan",
+    artist: "A.R Rahman/sriram",
+    img: "music_6",
+    src: "music_6"
+  },
+  {
+    name: "Jhoom",
+    artist: "Jalraj",
+    img: "music_7",
+    src: "music_7"
+  },
+  {
+    name: "Parayate Parayunna",
+    artist: "Nikhil chandran",
+    img: "music_8",
+    src: "music_8"
+  },
+  {
+    name: "nenjodu cherthu",
+    artist: "sreejith-saachin/aalaap raju",
+    img: "music_9",
+    src: "music_9"
+  },
+  {
+    name: "Badass(leo)",
+    artist: "Anirudh Ravichander",
+    img: "music_10",
+    src: "music_10"
+  },
+  {
+    name: "Mann Bharryaa",
+    artist: "B praak",
+    img: "music_11",
+    src: "music_11"
+  },
+  {
+  name: "lshq wala love",
+  artist: "vishal&shekhar/salim sadruddin merch",
+  img: "music_12",
+  src: "music_12"
+  },
+  {
+  name: "malare",
+  artist: "vijay yasudas",
+  img: "music_13",
+  src: "music_13"
+  },
+  {
+  name: "manju mazha(female version)",
+  artist: "shreya ghoshal",
+  img: "music_14",
+  src: "music_14"
+  },
+  {
+  name: "imakal chimmathiravum",
+  artist: "unkonow artist",
+  img: "music_15",
+  src: "music_15"
+  },
+  {
+  name: "karu karu karupayi",
+  artist: "deva muse/uni menon/anuradha sriram",
+  img: "music_16",
+  src: "music_16"
+  },
+  {
+  name: "efflovescence",
+  artist: "far&beyond",
+  img: "music_17",
+  src: "music_17"
+  },
+  {
+  name: "bloody sweet",
+  artist: "anirudh ravichander/siddharth basrur",
+  img: "music_18",
+  src: "music_18"
+  },
+  {
+  name: "glimpse of harold das",
+  artist: "anirudh ravichander",
+  img: "music_19",
+  src: "music_19"
+  },
+  {
+  name: "chand sifarish-lofi",
+  artist: "jatin-lalit/shaan/kailash kher/prasoon joshi",
+  img: "music_20",
+  src: "music_20"
+  },
+  {
+  name: "lut gaye",
+  artist: "jobinnautiyal/tanishk/rochak kohli",
+  img: "music_21",
+  src: "music_21"
+  },
+  {
+  name: "glimpse of antony das",
+  artist: "anirudh ravichander",
+  img: "music_22",
+  src: "music_22"
+  },
+  {
+  name: "sawan aaya hai",
+  artist: "arijit singh",
+  img: "music_23",
+  src: "music_23"
+  },
+  {
+  name: "en jeevan",
+  artist: "hariharan,saindhari,vaikam vijayalakshmi",
+  img: "music_24",
+  src: "music_24"
+  },
+  {
+  name: "lokiverse",
+  artist: "anirudh ravichander",
+  img: "music_25",
+  src: "music_25"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
